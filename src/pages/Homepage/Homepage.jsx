4a784962334864e2c96528dd4edbc0ed65f86333
hp <riteshgharti121232@gmail.com>
2025-02-04 09:45:52 +0530

    add button design

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,27 +1,25 @@
-import BannerBottom from '../../components/BannerBottom/BannerBottom'
-import BottomItemLists from '../../components/BottomItemLists/BottomItemLists'
-import HomeBanner from '../../components/HomeBanner/HomeBanner'
-import ItemLists from '../../components/ItemLists/ItemLists'
-import './Homepage.scss'
+import BannerBottom from "../../components/BannerBottom/BannerBottom";
+import BottomItemLists from "../../components/BottomItemLists/BottomItemLists";
+import HomeBanner from "../../components/HomeBanner/HomeBanner";
+import ItemLists from "../../components/ItemLists/ItemLists";
+import "./Homepage.scss";
 
 const Homepage = () => {
   return (
-    <div className='homepage'>
-  <HomeBanner />
+    <div className="homepage">
+      <HomeBanner />
 
-  <div className="categories-lists">
-    <ItemLists />
-  </div>
+      <div className="categories-lists">
+        <ItemLists />
+      </div>
 
-  <BannerBottom />
+      <BannerBottom />
 
-  <div className="categories-bottom-lists">
-    <BottomItemLists />
-  </div>
+      <div className="categories-bottom-lists">
+        <BottomItemLists />
+      </div>
     </div>
+  );
+};
 
-
-  )
-}
-
-export default Homepage
+export default Homepage;
