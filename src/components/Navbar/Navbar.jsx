2d041a9460a4b85bb5908cdeb4471cb0b9e5091a
hp <riteshgharti121232@gmail.com>
2025-02-06 14:30:39 +0530

    remove sidebar file

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,50 +9,48 @@ const Navbar = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   return (
-    <div className="navbar-container">
-      <div className="navbar">
-        <div className="nav-left">
-          <div className="logo">
-            <Link to={"/"}>
-              <h1>LOGO</h1>
-            </Link>
-          </div>
-        </div>
-
-        <div className="mobile-menu">
-          <MobileMenu />
-        </div>
-
-        <div className="nav-right">
-          <div
-            className="dropdown"
-            onMouseEnter={() => setIsDropdownOpen(true)}
-            onMouseLeave={() => setIsDropdownOpen(false)}
-          >
-            <p className="nav-link">Services</p>
-
-            {isDropdownOpen && (
-              <div className="dropdown-content">
-                {nav_items.map((item, index) => (
-                  <Link
-                    key={index}
-                    to={`/${item.nav_items_link}`}
-                    className="dropdown-link"
-                  >
-                    {item.nav_items_name}
-                  </Link>
-                ))}
-              </div>
-            )}
-          </div>
-
-          <Link to={"/about-us"} className="nav-link">
-            About Us
-          </Link>
-          <Link to={"/contact-us"} className="nav-link">
-            Contact Us
+    <div className="navbar">
+      <div className="nav-left">
+        <div className="logo">
+          <Link to={"/"}>
+            <h1>LOGO</h1>
           </Link>
         </div>
+      </div>
+
+      <div className="mobile-menu">
+        <MobileMenu />
+      </div>
+
+      <div className="nav-right">
+        <div
+          className="dropdown"
+          onMouseEnter={() => setIsDropdownOpen(true)}
+          onMouseLeave={() => setIsDropdownOpen(false)}
+        >
+          <p className="nav-link">Services</p>
+
+          {isDropdownOpen && (
+            <div className="dropdown-content">
+              {nav_items.map((item, index) => (
+                <Link
+                  key={index}
+                  to={`/${item.nav_items_link}`}
+                  className="dropdown-link"
+                >
+                  {item.nav_items_name}
+                </Link>
+              ))}
+            </div>
+          )}
+        </div>
+
+        <Link to={"/about-us"} className="nav-link">
+          About Us
+        </Link>
+        <Link to={"/contact-us"} className="nav-link">
+          Contact Us
+        </Link>
       </div>
     </div>
   );
